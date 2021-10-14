f41b6ba9a82c699451b823a97c0ad2a58740d295
Ewelina <ewelina2502@gmail.com>
2021-10-14 16:05:16 +0200

    fixture + refactor code

--- models/booking.py
+++ models/booking.py
@@ -2,6 +2,34 @@ import random
 import requests
 import url
 
+id_booking = 1
+
+
+def get_ids_booking():
+    response_get_ids = requests.get(f'{url.get_ids}')
+    assert response_get_ids.json()
+    get_ids = response_get_ids.json()
+    print(get_ids)
+
+
+def test_get_id():
+    response_get_id = requests.get(f'{url.get_id}' + f'{id_booking}')
+    assert response_get_id.json()
+    get_id = response_get_id.json()
+    print(get_id)
+
+
+def test_get_id_details():
+    response_get_id = requests.get(f'{url.get_id}' + f'{id_booking}')
+    assert response_get_id.status_code == 200
+    get_details = response_get_id.json()['totalprice']
+    firstname = response_get_id.json()['firstname']
+    lastname = response_get_id.json()['lastname']
+    additionalneeds = response_get_id.json()['additionalneeds']
+    print(
+        '{', '"total": ', get_details, ",", '"firstname": ', '"', firstname, '"', ",", '"lastname": ', '"',
+        lastname, '"', ",", '"additionalneeds":', '"', additionalneeds, '"', '}')
+
 
 class Bookings:
 
@@ -29,6 +57,5 @@ class Bookings:
 
         response_add_booking = requests.post(url.post_add_booking, json=body)
         assert response_add_booking.status_code == 200
-        add_booking = response_add_booking.json()
+        add_booking = response_add_booking.json()['bookingid']
         print(add_booking)
-
--- tests/test_get_post.py
+++ tests/test_get_post.py
@@ -1,39 +1,31 @@
-import requests
-import url
-from models.booking import Bookings
+import pytest
 
-id_booking = 3
+from models import booking
+from models.booking import Bookings
 
 
 def test_get_ids():
-    response_get_ids = requests.get(f'{url.get_ids}')
-    assert response_get_ids.json()
-    get_ids = response_get_ids.json()
-    print(get_ids)
-
-
-def test_get_id():
-    response_get_id = requests.get(f'{url.get_id}' + f'{id_booking}')
-    assert response_get_id.json()
-    get_id = response_get_id.json()
-    print(get_id)
-
-
-def test_get_id_details():
-    response_get_id = requests.get(f'{url.get_id}' + f'{id_booking}')
-    assert response_get_id.status_code == 200
-    get_details = response_get_id.json()['totalprice']
-    firstname = response_get_id.json()['firstname']
-    lastname = response_get_id.json()['lastname']
-    additionalneeds = response_get_id.json()['additionalneeds']
-    print(
-        '{', '"total": ', get_details, ",", '"firstname": ', '"', firstname, '"', ",", '"lastname": ', '"',
-        lastname, '"', ",", '"additionalneeds":', '"', additionalneeds, '"', '}'
-    )
+    get_ids = booking
+    get_ids.get_ids_booking()
 
 
 def test_add_booking():
     add_booking = Bookings()
     add_booking.add_booking()
 
+
+def test_get_id():
+    get_id = booking
+    get_id.test_get_id()
+
+
+def test_get_id_details(new_booking):
+    get_id_details = booking
+    get_id_details.test_get_id_details()
+
+
+@pytest.fixture
+def new_booking():
+    new_booking = Bookings()
+    return new_booking.add_booking()
 
