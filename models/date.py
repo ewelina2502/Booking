b1ee507c5d10b4bee818c7d759184a238ea7acab
Ewelina <ewelina2502@gmail.com>
2021-12-14 17:05:34 +0100

    add hour in date file

--- models/date.py
+++ models/date.py
@@ -1,4 +1,13 @@
-from datetime import date
+from datetime import date, datetime
 
 date_today = date.today()
 print(date_today)
+
+
+now = datetime.now()
+
+dt_string = now.strftime("%d-%m-%Y %H:%M:%S")
+print("date and time =", dt_string)
+
+dt2_string = now.strftime("%H:%M:%S")
+print("time =", dt2_string)
